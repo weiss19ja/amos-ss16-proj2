c3b8c30420ca2d9b55819d809a353524b5ace2cb
Valerie Wiedemann <vale92@arcor.de>
2016-05-09 21:48:31 +0200

    Video stream appears on canvas
    You have to start the server manually in order to get the stream

--- webapp/roverObserve/roverObserve.js
+++ webapp/roverObserve/roverObserve.js
@@ -1,42 +1,56 @@
 'use strict';
 
 angular.module('myApp.roverObserve', [])
-    .factory('squareDrawer', function() {
-        var registry = {};
-
-        var squareDrawer = {
+    .factory('streamDrawer', function() {
+        var streamDrawer = {
             /**
-             * draw the square on the canvas
+             * resize the canvas
              */
-            draw: function(height, width) {
-                console.log("square to draw: " + height + "x" + width);
+            updateSize: function(height, width) {
+                console.log("Change canvas size to: " + height + "x" + width);
 
                 var canvas = document.getElementById("canvas");
                 if (canvas.getContext) {
-                    console.log("drawing");
                     var ctx = canvas.getContext("2d");
-                    canvas.width  = width;
-                    canvas.height = height;
                     canvas.style.width  = width+'px';
                     canvas.style.height = height+'px';
-                    //clear the canvas
-                    ctx.clearRect(0,0, 200, canvas.height);
-                    ctx.fillRect(0, 0, 1000, 1000);
                 }
             }
         };
-        return squareDrawer;
+        return streamDrawer;
     })
-.controller('RoverObserveCtrl', function($scope, squareDrawer) {
-    $scope.squareDrawer = squareDrawer;
-    $scope.square = {
-        height: '100',
-        width: '100'
+.controller('RoverObserveCtrl', function($scope, streamDrawer) {
+    $scope.streamDrawer = streamDrawer;
+    $scope.canvasSize = {
+        width: '640',
+        height: '480'
     };
-    $scope.$watch('square', function(newValue, oldValue) {
+    $scope.$watch('canvasSize', function(newValue, oldValue) {
         if (newValue != oldValue) {
-            console.log('value changed: ' + JSON.stringify($scope.square));
-            squareDrawer.draw(newValue.height, newValue.width);
+            //console.log('value changed: ' + JSON.stringify($scope.square));
+            streamDrawer.updateSize(newValue.height, newValue.width);
         }
     }, true);
+    var init = function(){
+        var canvas = document.getElementById("canvas");
+        if (canvas.getContext) {
+            console.log("init canvas");
+            var ctx = canvas.getContext("2d");
+            // set initial canvas size
+            var width = $scope.canvasSize.width;
+            var height = $scope.canvasSize.height;
+            canvas.width  = width;
+            canvas.height = height;
+            canvas.style.width  = width+'px';
+            canvas.style.height = height+'px';
+            //clear the canvas
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillText('Loading video stream...', canvas.width / 3 , canvas.height / 3);
+            // Setup the WebSocket connection and start the player
+            var client = new WebSocket('ws://192.168.0.8:8084/');
+            var player = new jsmpeg(client, {canvas: canvas});
+        }
+
+    };
+    init();
 });
