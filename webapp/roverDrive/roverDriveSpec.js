6220045326e3adff8e370fc7dc5da98cef0545fb
Valerie Wiedemann <vale92@arcor.de>
2016-05-09 18:16:56 +0200

    Added a canvas element to the application
    The canvas element is later on used to display the camera-stream

--- webapp/roverDrive/roverDriveSpec.js
+++ webapp/roverDrive/roverDriveSpec.js
@@ -2,13 +2,14 @@
 
 describe('myApp.roverDrive module', function() {
 
+  var scope;
   beforeEach(module('myApp.roverDrive'));
 
   describe('roverDrive controller', function(){
 
-    it('should ....', inject(function($controller) {
+    it('should ....', inject(function($controller, $rootScope) {
       //spec body
-      var roverDriveCtrl = $controller('RoverDriveCtrl');
+      var roverDriveCtrl = $controller('RoverDriveCtrl', {$scope: scope});
       expect(roverDriveCtrl).toBeDefined();
     }));
 
--- webapp/roverObserve/roverObserve.js
+++ webapp/roverObserve/roverObserve.js
@@ -1,8 +1,38 @@
 'use strict';
 
 angular.module('myApp.roverObserve', [])
+    .factory('squareDrawer', function() {
+        var registry = {};
 
+        var squareDrawer = {
+            /**
+             * draw the square on the canvas
+             */
+            draw: function(height, width) {
+                console.log("square to draw: " + height + "x" + width);
 
-.controller('RoverObserveCtrl', ['$scope',function($scope) {
-
-}]);
+                var canvas = document.getElementById("canvas");
+                if (canvas.getContext) {
+                    console.log("drawing");
+                    var ctx = canvas.getContext("2d");
+                    //clear the canvas
+                    ctx.clearRect(0,0, 200, canvas.height);
+                    ctx.fillRect(0, 0, width, height);
+                }
+            }
+        };
+        return squareDrawer;
+    })
+.controller('RoverObserveCtrl', function($scope, squareDrawer) {
+    $scope.squareDrawer = squareDrawer;
+    $scope.square = {
+        height: '100',
+        width: '100'
+    };
+    $scope.$watch('square', function(newValue, oldValue) {
+        if (newValue != oldValue) {
+            console.log('value changed: ' + JSON.stringify($scope.square));
+            squareDrawer.draw(newValue.height, newValue.width);
+        }
+    }, true);
+});
