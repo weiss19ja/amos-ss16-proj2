bbe3c79174cd3dab523d308461b738d44f0c0c08
Maximilian Fischer <maximilian.fischer1991@gmail.com>
2016-05-12 17:45:57 +0200

    added javadoc for collision controler and remapped gpio pins of rover

--- backend/src/main/java/de/developgroup/mrf/rover/collision/CollisionController.java
+++ backend/src/main/java/de/developgroup/mrf/rover/collision/CollisionController.java
@@ -1,5 +1,13 @@
 package de.developgroup.mrf.rover.collision;
 
+/**
+ * The collision controller is responsible for the collision detection of the IR
+ * sensors of the rover. The rover has 4 ir sensors, one for each corner,
+ * labeled front right, front left, back right and back left.
+ * 
+ * See concrete wiring of GPIO pins in github wiki:
+ * https://github.com/weiss19ja/amos-ss16-proj2/wiki/Hardware-GPIO-Mapping
+ */
 public interface CollisionController {
 
 	public static final String RECEIVER_FRONT_RIGHT = "IRReceiverFrontRight";
@@ -11,12 +19,32 @@ public interface CollisionController {
 	public static final String SENDER_BACK_LEFT = "IRSenderBackLeft";
 	public static final String SENDER_BACK_RIGHT = "IRSenderBackRight";
 
+	/**
+	 * Methods returns sensor value of the front right corner.
+	 * 
+	 * @return true if collision detected
+	 */
 	public boolean hasCollisionFrontRight();
 
+	/**
+	 * Methods returns sensor value of the front left corner.
+	 * 
+	 * @return true if collision detected
+	 */
 	public boolean hasCollisionFrontLeft();
 
+	/**
+	 * Methods returns sensor value of the back left corner.
+	 * 
+	 * @return true if collision detected
+	 */
 	public boolean hasCollisionBackLeft();
 
+	/**
+	 * Methods returns sensor value of the back right corner.
+	 * 
+	 * @return true if collision detected
+	 */
 	public boolean hasCollisionBackRight();
 
 }
