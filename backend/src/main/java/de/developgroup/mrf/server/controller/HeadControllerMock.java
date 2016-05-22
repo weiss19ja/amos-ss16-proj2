461a464a3e2198ed99a277c17dafac0d16626c31
Valerie Wiedemann <vale92@arcor.de>
2016-05-22 17:11:15 +0200

    Refactoring mockup

---
+++ backend/src/main/java/de/developgroup/mrf/server/controller/HeadControllerMock.java
@@ -0,0 +1,42 @@
+package de.developgroup.mrf.server.controller;
+
+import org.cfg4j.provider.ConfigurationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class HeadControllerMock implements HeadController{
+
+    private static Logger LOGGER = LoggerFactory.getLogger(HeadControllerMock.class);
+
+    @Override
+    public void initialize(ConfigurationProvider configurationProvider) throws IOException {
+        LOGGER.info("initializing HeadController");
+    }
+
+    @Override
+    public void turnHeadUp(int angle) {
+        LOGGER.debug("Turning head up");
+    }
+
+    @Override
+    public void turnHeadDown(int angle) {
+        LOGGER.debug("Turning head down");
+    }
+
+    @Override
+    public void turnHeadLeft(int angle) {
+        LOGGER.debug("Turning head left");
+    }
+
+    @Override
+    public void turnHeadRight(int angle) {
+        LOGGER.debug("Turning head right");
+    }
+
+    @Override
+    public void stopHead() {
+        LOGGER.debug("Stopping head");
+    }
+}
