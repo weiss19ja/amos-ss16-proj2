c7fd11a218ca35d759ad1ec01f1fc341df96afe7
Jan Weiß <weiss19ja@gmail.com>
2016-05-29 19:24:18 +0200

    req16: added a floating action button in the toolbar for the camera snapshot

--- webapp/roverObserve/roverObserve.js
+++ webapp/roverObserve/roverObserve.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('myApp.roverObserve', [])
-.controller('RoverObserveCtrl', ['$scope', '$location',function($scope, $location) {
+.controller('RoverObserveCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
     $scope.mjpegStreamURL = 'http://' + $location.host() + ':9000/stream/video.mjpeg';
+    //$scope.mjpegStreamURL = 'http://192.168.188.38:9000/stream/video.mjpeg';
+
+    $scope.snapshotEnabled = true;
+
+    $scope.snapshotClicked = function(clickEvent) {
+        $http({
+            method: 'GET',
+            url: 'http://' + $location.host() + ':9000/stream/snapshot.jpeg'
+        }).then(function successCallback(response) {
+            console.log("Statuscode: " + response.status + " \nData: " + response.data);
+        }, function errorCallback(response) {
+            console.log(response);
+        });
+
+    };
 }]);
