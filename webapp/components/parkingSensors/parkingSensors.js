5a23648317da07c4be43fc8feb523655cc2af32e
Martin Hofmann <m-hofmann@users.noreply.github.com>
2016-05-29 10:58:10 +0200

    Bind SVG elements' visibility to Controller

--- webapp/components/parkingSensors/parkingSensors.js
+++ webapp/components/parkingSensors/parkingSensors.js
@@ -1,13 +1,26 @@
 'use strict';
 
 angular.module('myApp.parkingSensors', [])
-.controller('ParkingSensorsController', ['$scope', 'roverService', '$attrs', function($scope, roverService, $attrs) {
+    .controller('ParkingSensorsController', ['$scope', 'roverService', '$attrs', function($scope, roverService, $attrs) {
+        $scope.collisions = {
+            frontLeft: true,
+            frontRight: false,
+            backLeft: true,
+            backRight: false
+        };
 
-
-}])
-.component('parkingSensors', {
-    restrict: 'EA',
-    templateUrl: 'components/parkingSensors/parkingSensors.html',
-    css: 'components/parkingSensors/parkingSensors.css',
-    controller: 'ParkingSensorsController'
-});
+        $scope.toggle = function() {
+            $scope.collisions = {
+                frontLeft: !$scope.collisions.frontLeft,
+                frontRight: !$scope.collisions.frontRight,
+                backLeft: !$scope.collisions.backLeft,
+                backRight: !$scope.collisions.backRight
+            };
+        };
+    }])
+    .component('parkingSensors', {
+        restrict: 'EA',
+        templateUrl: 'components/parkingSensors/parkingSensors.html',
+        css: 'components/parkingSensors/parkingSensors.css',
+        controller: 'ParkingSensorsController'
+    });
