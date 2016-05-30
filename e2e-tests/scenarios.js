d82ee6f145c342e6352d28e3a34c3dd84f5e935d
Andreas Bauer <andreas.bauer200@gmail.com>
2016-05-30 18:42:28 +0200

    Merge branch 'iss15-e2e-tests' Fix for issue #15 "Protractor e2e tests not running on Travis CI". Implementing together with Maximilian Fischer.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -44,7 +44,7 @@ describe ('Main view', function() {
     mainViewButtons.get(1).click();
     expect(browser.getLocationAbsUrl()).toMatch("/observe");
   });
-  
+
 });
 
 describe ('Drive view', function() {
@@ -144,26 +144,30 @@ describe ('sidebar navigation', function() {
   it('should be redirect to main page when driver is Main', function() {
     expect (sidebarItems.get(0).getText()).toBe('Main');
     sidebarItems.get(0).click();
+    browser.refresh();
     expect(browser.getLocationAbsUrl()).toMatch("/main");
   });
 
-  xit('should be redirect to drive page when driver is clicked', function() {
+  it('should be redirect to drive page when driver is clicked', function() {
     expect (sidebarItems.get(1).getText()).toBe('Driver');
     sidebarItems.get(1).click();
+    browser.refresh(); 
     expect(browser.getLocationAbsUrl()).toMatch("/drive");
   });
 
-  xit('should be redirect to observe page when Observer is clicked', function() {
+  it('should be redirect to observe page when Observer is clicked', function() {
     expect (sidebarItems.get(2).getText()).toBe('Observer');
     sidebarItems.get(2).click();
+    browser.refresh();
     expect(browser.getLocationAbsUrl()).toMatch("/observe");
   });
 
-  xit('should be redirect to settings page when Settings is clicked', function() {
+  it('should be redirect to settings page when Settings is clicked', function() {
     expect (sidebarItems.get(3).getText()).toBe('Settings');
     sidebarItems.get(3).click();
+    browser.refresh();
     expect(browser.getLocationAbsUrl()).toMatch("/settings");
   });
-  
+
 
 });
