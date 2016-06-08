f2c1cad3a38d58ab28c5459c4a45998bd5a7fb63
Maximilian Fischer <maximilian.fischer1991@gmail.com>
2016-06-08 23:02:31 +0200

    added roverDrive unitTests in frontend. Implemented in collaboration with Andreas Bauer.

--- webapp/roverDrive/roverDriveSpec.js
+++ webapp/roverDrive/roverDriveSpec.js
@@ -6,21 +6,31 @@ describe('myApp.roverDrive module', function () {
   beforeEach(module('myApp.roverService'));
 
   describe('should define roverDriveController', function () {
-    var scope, roverDriveCtrl;
+    var scope, roverDriveCtrl, $location, roverService;
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    beforeEach(inject(function($rootScope, $controller, _$location_, _roverService_) {
       scope = $rootScope.$new();
       roverDriveCtrl = $controller('RoverDriveCtrl', {$scope: scope});
-
+      $location = _$location_;
+      roverService = _roverService_;
     }));
 
     it('should define the Developer Controller', function () {
       expect(roverDriveCtrl).toBeDefined();
     });
 
-    // test routeView on Desktop devices
-    // call of enter and exit driver mode
+    it('should call enterDriverMode when /drive is opened', function () {
+      spyOn(roverService, 'enterDriverMode');
+      $location.path('/drive');
+      setTimeout(function () {expect(roverService.enterDriverMode).toHaveBeenCalled()}, 250);
+    });
 
+    it('should call exitDriverMode when /drive is closed', function () {
+      spyOn(roverService, 'exitDriverMode');
+      $location.path('/observe');
+      setTimeout(function () {expect(roverService.exitDriverMode).toHaveBeenCalled()}, 250);
+    });
+    
 
 
   });
