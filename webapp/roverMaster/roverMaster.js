8e00b3573b668ada67516262a3788509e5982bb4
Martin Hofmann <m-hofmann@users.noreply.github.com>
2016-06-11 12:21:40 +0200

    Add skeleton for roverMaster view
    - empty HTML page
    - controller skeleton
    - wire the page to the Angular Routing facility

---
+++ webapp/roverMaster/roverMaster.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('myApp.roverMaster', [])
+.controller('RoverMasterCtrl', ['roverService', '$scope', '$location', '$mdMedia',
+  function(roverService, $scope, $location, $mdMedia) {
+
+  }]);
