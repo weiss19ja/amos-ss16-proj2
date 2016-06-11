69d27ee794ffbd3040bb2b2074e1d83fdfe4e36f
Martin Hofmann <m-hofmann@users.noreply.github.com>
2016-06-11 14:35:53 +0200

    Fix roverMaster camera stream URL
    The $location.protocol() function was not correctly called. It is now.

--- webapp/roverMaster/roverMaster.js
+++ webapp/roverMaster/roverMaster.js
@@ -3,10 +3,13 @@
 angular.module('myApp.roverMaster', [])
 .controller('RoverMasterCtrl', ['roverService', '$scope', '$location', '$mdMedia',
   function(roverService, $scope, $location, $mdMedia) {
-    $scope.mjpegStreamURL = $location.protocol + $location.host() + ':9000/stream/video.mjpeg';
+    $scope.mjpegStreamURL = $location.protocol() + '://' + $location.host() + ':9000/stream/video.mjpeg';
     $scope.roverState = roverService.roverState;
 
     console.log('Enter Driver Mode (from roverMaster)');
+    /**
+     * As soon as the view is opened, we try to register this user as a driver.
+     */
     roverService.enterDriverMode();
 
     $scope.stop = function() {
