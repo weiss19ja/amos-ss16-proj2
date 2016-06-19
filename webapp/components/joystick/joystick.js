3dd12c3fa35ce1e85c156025d58db9d3e9986ffd
Andreas Bauer <andreas.bauer200@gmail.com>
2016-06-19 18:14:59 +0200

    implement joystick directive

--- webapp/components/joystick/joystick.js
+++ webapp/components/joystick/joystick.js
@@ -10,18 +10,38 @@
 angular.module('myApp.joystick', [])
   .controller('JoystickCtrl', ['$scope', 'roverService','$attrs', function($scope, roverService, $attrs) {
 
-    $scope.joystickSize;
-
-    setSize();
-
-
-    function setSize() {
+    function init() {
       if($attrs.size){
         $scope.joystickSize = $attrs.size;
       } else {
         $scope.joystickSize = 150;
       }
     }
+
+    init();
+
+    $scope.joystickSize;
+
+    $scope.up = function () {
+      roverService.driveForward();
+    };
+
+    $scope.down = function () {
+      roverService.driveBackward();
+    };
+
+    $scope.left = function () {
+      roverService.turnLeft();
+    };
+
+    $scope.right = function () {
+      roverService.turnRight();
+    };
+
+    $scope.stop = function () {
+      roverService.stop();
+    };
+
 
     var options = {
       zone: document.getElementById('zone_joystick'),
@@ -34,14 +54,33 @@ angular.module('myApp.joystick', [])
     var manager = nipplejs.create(options);
 
     manager.on('move', function (evt, data) {
-      //console.log(data);
-      console.log('radian'+data.angle.radian +'   distance: '+data.distance);
-      //console.log('x: '+data.position.x +'   y: '+data.position.y);
-      //console.log('manager x: '+options.position.x +'   y: '+options.position.y);
+      //console.log('radian'+data.angle.radian +'   distance: '+data.distance);
+    });
+
+    manager.on('dir:up',function (evt,data) {
+      console.log('joystick:up');
+      $scope.up();
+    });
+
+    manager.on('dir:down',function (evt,data) {
+      console.log('joystick:down');
+      $scope.down();
+    });
+
+    manager.on('dir:left',function (evt,data) {
+      console.log('joystick:left');
+      $scope.left();
+    });
+
+    manager.on('dir:right',function (evt,data) {
+      console.log('joystick:right');
+      $scope.right();
+
     });
 
     manager.on('end',function (evt,data) {
-      console.log('halt stop');
+      console.log('joystick:stop');
+      $scope.stop();
     });
     
   }])
