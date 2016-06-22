89eaf91bf919cbacb2c4a343180c6aab9af7d1f5
Jan Weiß <weiss19ja@gmail.com>
2016-06-22 23:07:41 +0200

    Implemented req63 laptop-ui-option camera movement and stream on desktop

--- webapp/roverObserve/roverObserve.js
+++ webapp/roverObserve/roverObserve.js
@@ -1,40 +1,32 @@
 'use strict';
 
 angular.module('myApp.roverObserve', ['ngMaterial'])
-.controller('RoverObserveCtrl', ['roverService', '$scope', '$location', '$mdDialog', '$mdMedia', function(roverService, $scope, $location, $mdDialog, $mdMedia) {
-    $scope.mjpegStreamURL = 'http://' + $location.host() + ':9000/stream/video.mjpeg';
-    $scope.snapshotEnabled = true;
-    checkRedirect();
+    .controller('RoverObserveCtrl', ['roverService', '$scope', '$location', '$mdDialog', '$mdMedia', function (roverService, $scope, $location, $mdDialog, $mdMedia) {
+      $scope.mjpegStreamURL = 'http://' + $location.host() + ':9000/stream/video.mjpeg';
+      $scope.snapshotEnabled = true;
 
-    $scope.snapshotClicked = function(clickEvent) {
+      $scope.snapshotClicked = function (clickEvent) {
         roverService.getCameraSnapshot(function (imageData) {
 
-            $mdDialog.show({
-                controller: SnapshotDialogController,
-                templateUrl: 'roverObserve/snapshotDialog.html',
-                parent: angular.element(document.body),
-                targetEvent: clickEvent,
-                clickOutsideToClose: true,
-                locals: {
-                    imageUrl: imageData[0]
-                }
-            });
+          $mdDialog.show({
+            controller: SnapshotDialogController,
+            templateUrl: 'roverObserve/snapshotDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: clickEvent,
+            clickOutsideToClose: true,
+            locals: {
+              imageUrl: imageData[0]
+            }
+          });
 
         });
-    };
+      };
 
-    function SnapshotDialogController($scope, $mdDialog, imageUrl) {
+      function SnapshotDialogController($scope, $mdDialog, imageUrl) {
         $scope.imageUrl = imageUrl;
-        $scope.cancel = function() {
-            $mdDialog.cancel();
+        $scope.cancel = function () {
+          $mdDialog.cancel();
         }
-    }
+      }
 
-    function checkRedirect() {
-        if($location.path().indexOf('cameraController') > -1) {
-            if($mdMedia('gt-md')) {
-                $location.path('/main');
-            }
-        }
-    }
-}]);
+    }]);
